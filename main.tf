e9a4fbadf0bb2cfb4f25261015fb03247066fe21
Robert Tillery <rtillery@hashicorp.com>
2019-06-25 15:27:08 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "tfe" {
+ hostname = "${var.hostname}"
+ version = "<= 0.9.1"
+}
+
+resource "tfe_oauth_client" "oauth" {
+  organization = "${var.organization}"
+  api_url = "https://api.github.com"
+  http_url = "https://github.com"
+  oauth_token = "${var.oauth_token}"
+  service_provider = "github"
+}
+
+ resource "tfe_workspace" "tfr" {
+   count = "${var.workspace_count}"
+   organization = "${var.organization}"
+   name = "tfr${count.index}"
+   auto_apply = true
+   queue_all_runs = true
+
+   vcs_repo {
+     identifier = "${var.repo}"
+     branch = "${var.branch}"
+     oauth_token_id = "${tfe_oauth_client.oauth.oauth_token_id}"
+   }
+ }
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "hostname" {
+  type = "string"
+}
+
+variable "oauth_token" {
+  type = "string"
+}
+
+variable "organization" {
+  type = "string"
+}
+
+variable "workspace_count" {
+  type = "string"
+}
+
+variable "branch" {
+  type = "string"
+}
+
+variable "repo" {
+  type = "string"
+}
