198951a02e71bd577abd774c57c5f32918b4dbd0
Rexredinger <redinger@hashicorp.com>
2022-08-08 10:34:42 -0400

    child workspace init

--- main.tf
+++ main.tf
@@ -1,56 +1,9 @@
-terraform {
-  required_providers {
-    tfe = {
-      version = "~> 0.31.0"
-    }
-  }
-}
-
 provider "tfe" {
   hostname = var.hostname
 }
 
-resource "tfe_oauth_client" "oauth" {
-  organization     = var.organization
-  api_url          = "https://api.github.com"
-  http_url         = "https://github.com"
-  oauth_token      = var.github_oauth_token
-  service_provider = "github"
+resource "tfe_workspace" "initiald" {
+  organization = var.organization
+  name = "Initial_D"
 }
 
-resource "tfe_workspace" "parent_workspace" {
-  organization      = var.organization
-  name              = "ParentWorkspace"
-  auto_apply        = false
-  queue_all_runs    = true
-
-  vcs_repo {
-    identifier     = var.repo
-    branch         = var.branch
-    oauth_token_id = tfe_oauth_client.oauth.oauth_token_id
-  }
-}
-
-resource "tfe_variable" "token" {
-  key="TFE_TOKEN"
-  value=var.tfe_token
-  category="env"
-  sensitive=true
-  workspace_id=tfe_workspace.parent_workspace.id
-  description="This allows the build agent to call back to TFC when executing plans and applies"
-}
-
-resource "tfe_variable" "organization" {
-  key="organization"
-  value=var.organization
-  category="terraform"
-  workspace_id=tfe_workspace.parent_workspace.id
-  description="Passing along the var setting from this config to the config that workspace parent_workspace will use to generate the second workspace"
-}
-
-resource "tfe_variable" "hostname" {
-  key="hostname"
-  value=var.hostname
-  category="terraform"
-  workspace_id=tfe_workspace.parent_workspace.id
-}
--- variables.tf
+++ variables.tf
@@ -1,26 +1,7 @@
-variable "hostname" {
-  type = string
-}
-
-variable "github_oauth_token" {
-  type = string
-}
-
 variable "organization" {
   type = string
-  default = "workspaces_managerment"
 }
 
-variable "branch" {
-  type = string
-  default = "workspace_child"
-}
-
-variable "repo" {
-  type = string
-  default = "rexredinger/tfe-terraform"
-}
-
-variable "tfe_token" {
+variable "hostname" {
   type = string
 }
